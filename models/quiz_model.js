a828783861c4b0ccd58944958f074f8425cd9400
Borja <alu0100498820@ull.edu.es>
2015-11-10 15:23:47 +0000

    Realizando modificaciones en quiz_model.js

--- models/quiz_model.js
+++ models/quiz_model.js
@@ -53,8 +53,8 @@ function Quiz() {
     { /* Código inseguro. ¡No ejecute esta pregunta salvo en un
          entorno en el que el código del "alumno" sea fiable!
        */
-      pregunta: 'Escriba una función JavaScript de nombre <tt>square</tt> '+
-      'que recibe un número y devuelve el cuadrado',
+  pregunta: 'Escriba una función JavaScript de nombre <tt>square</tt> '+ 
+  'que recibe un número y devuelve el cuadrado',
       respuesta: function(x) {
         try {
           eval(x); /* DANGER DANGER DANGER */
@@ -68,26 +68,22 @@ function Quiz() {
       }
     }
   );
+
   // insertar unas cuantas preguntas sobre
   // la tabla de multiplicar
   var self  = this;
   for(var i = 0; i<3;i++) {
     (function() {
       var n1 = Math.randomInt(9)+1;
-      debug("n1 = "+n1);
       var n2 = Math.randomInt(9)+1;
-      debug("n2 = "+n2);
       self.q.push(
-        { pregunta: '¿ '+n1+'x'+n2+"= ?",
+        { pregunta: '¿ '+n1+'x'+n2+" ?",
           respuesta: function(x) {
-            debug("n1 = "+n1);
-            debug("n2 = "+n2);
             return (x == n1*n2);
         }
       })
     })();
   }
-  debug(this.q);
 }
 
 Quiz.prototype = new AbstractQuiz();
